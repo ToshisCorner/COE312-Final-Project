984868a52fe210dfe339dfe3137087d98a0a85f9
Abdul Malik <Abdul Malik@ThinkPad-E14>
2024-11-12 00:19:00 +0400

    TCP Class implemented (needs W_book implementation)

--- src/Supplement/TCP_Client.java
+++ src/Supplement/TCP_Client.java
@@ -1,6 +1,21 @@
 package Supplement;
 
-public class TCP_Client 
-{
+public class TCP_Client implements Runnable {
 
+	String host;
+	int port;
+
+	TCP_Client(String host, int port) {
+		this.host = host;
+		this.port = port;
+		Thread t = new Thread(this);
+		t.start();
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
 }
